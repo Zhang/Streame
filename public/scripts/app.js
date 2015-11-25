ac7802924d5feb326c9354da47e08a350f1bbe7c
Zhang <scottzhang235@gmail.com>
2015-11-24 19:35:00 -0800

    scrolls to bottom of chat

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,7 +13,22 @@
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
     $state.go('broadcast');
   });
-
+  app.directive('ngScrollBottom', ['$timeout', function ($timeout) {
+    return {
+      scope: {
+        ngScrollBottom: '='
+      },
+      link: function ($scope, el) {
+        $scope.$watchCollection('ngScrollBottom', function (newValue) {
+          if (newValue) {
+            $timeout(function(){
+              $(el).scrollTop(el[0].scrollHeight);
+            }, 0);
+          }
+        });
+      }
+    };
+  }]);
   app.factory('SocketManager', function(socketFactory) {
     var socket = socketFactory({
       ioSocket: io.connect()
